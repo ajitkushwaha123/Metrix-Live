1393fcaddd0495a51157d270d95d0a418a850e94
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-04 21:13:31 +0530

    Responsive

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -48,37 +48,63 @@ const Login = () => {
   });
 
   return (
-    <div className='w-[full] font-poppins flex h-screen justify-center items-center'>
-    <Toaster position='top-center' reverseOrder='false'></Toaster>
-    <form onSubmit={formik.handleSubmit}>
-      <div className='px-[40px] py-[40px] bg-white rounded-xl '>
-         <div className='flex justify-center items-center flex-col'>
-            <img width={"60px"} src={metrix}/>
-            <h3 className='pt-[15px] font-semibold text-[20px]'>Welcome back!</h3>
-            <p className='pb-[40px] pt-[5px]'>Login to your account</p>
-         </div>
-
-         <div className='w-[full] flex flex-col justify-center items-center'>
-            <div className='flex rounded-lg text-[18px] justify-center items-center bg-[#EFF1F9] w-[375px] h-[52px]'> 
-              <IoMailOutline />
-              <input {...formik.getFieldProps('username')} className='w-[303px] ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Email Address' type='username'/>
-            </div>
-
-            <div className='flex rounded-lg my-[20px] text-[18px] justify-center items-center bg-[#EFF1F9] w-[375px] h-[52px]'> 
-              <IoKeyOutline />
-              <input {...formik.getFieldProps('password')} className='w-[303px] ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Password' type='password'/>
-            </div>
-         </div>
-
-         <NavLink to={'/forget-password'}><p className='text-primary flex justify-end items-center px-[40px]'>Recover Password</p></NavLink>
-          <div className='flex justify-center items-center flex-col'>
-              <p className='py-[15px]'>Don’t have an account? <NavLink to={'/register'}><span className='text-primary'> Sign Up </span></NavLink> </p>
-              <button type="submit" className='bg-primary px-[20px] py-2 mt-[20px] rounded-md text-white text-[18px]'>Login</button>
+    <div className="w-full font-poppins flex justify-center items-center">
+      <Toaster position="top-center" reverseOrder="false"></Toaster>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="md:px-[40px] my-[30px] p-[20px] md:py-[40px] bg-white rounded-xl ">
+          <div className="flex justify-center items-center flex-col">
+            <img width={"60px"} src={metrix} />
+            <h3 className="pt-[15px] font-semibold text-[20px]">
+              Welcome back!
+            </h3>
+            <p className="pb-[40px] pt-[5px]">Login to your account</p>
           </div>
-      </div>
+
+          <div className="w-[full] flex flex-col justify-center items-center">
+            <div className="flex px-[15px]  rounded-lg text-[18px] justify-center items-center bg-[#EFF1F9] h-[52px]">
+              <IoMailOutline />
+              <input
+                {...formik.getFieldProps("username")}
+                className="ml-[10px] h-[36px] outline-none bg-[#EFF1F9]"
+                placeholder="Email Address"
+                type="username"
+              />
+            </div>
+
+            <div className="flex px-[15px] rounded-lg my-[20px] text-[18px] justify-center items-center bg-[#EFF1F9] h-[52px]">
+              <IoKeyOutline />
+              <input
+                {...formik.getFieldProps("password")}
+                className="ml-[10px] h-[36px] outline-none bg-[#EFF1F9]"
+                placeholder="Password"
+                type="password"
+              />
+            </div>
+          </div>
+
+          <NavLink to={"/forget-password"}>
+            <p className="text-primary flex justify-end items-center px-[40px]">
+              Recover Password
+            </p>
+          </NavLink>
+          <div className="flex justify-center items-center flex-col">
+            <p className="py-[15px]">
+              Don’t have an account?{" "}
+              <NavLink to={"/register"}>
+                <span className="text-primary"> Sign Up </span>
+              </NavLink>{" "}
+            </p>
+            <button
+              type="submit"
+              className="bg-primary px-[20px] py-2 mt-[20px] rounded-md text-white text-[18px]"
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </form>
     </div>
-  )
+  );
 }
 
 export default Login
--- src/Authentication/Register.jsx
+++ src/Authentication/Register.jsx
@@ -37,34 +37,34 @@ const Register = () => {
   })
 
   return (
-    <div className='w-[full] font-poppins flex h-screen justify-center items-center'>
+    <div className='w-full font-poppins flex justify-center items-center'>
     <Toaster position='top-center' reverseOrder='false'></Toaster>
     <form onSubmit={formik.handleSubmit}>
-      <div className='w-[433px] py-[40px] bg-white rounded-xl '>
+      <div className='py-[20px] px-[20px] md:px-[30px] my-[30px] md:py-[40px] bg-white rounded-xl '>
          <div className='flex justify-center items-center flex-col'>
             <img width={"60px"} src={metrix}/>
             <h3 className='pt-[15px] font-semibold text-[20px]'>Welcome back!</h3>
             <p className='pb-[40px] pt-[5px]'>Register to your account</p>
          </div>
 
-         <div className='w-[full] flex flex-col justify-center items-center'>
-            <div className='flex rounded-lg text-[18px] mb-[20px] justify-center items-center bg-[#EFF1F9] w-[375px] h-[52px]'> 
+         <div className='w-full flex flex-col justify-center items-center'>
+            <div className='flex px-[15px] md:px-[20px] rounded-lg text-[18px] mb-[20px] justify-center items-center bg-[#EFF1F9] h-[52px]'> 
               <CiUser />
-              <input {...formik.getFieldProps('username')} className='w-[303px] ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Full Name' type='text'/>
+              <input {...formik.getFieldProps('username')} className='ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Full Name' type='text'/>
             </div>
 
-            <div className='flex rounded-lg text-[18px] justify-center items-center bg-[#EFF1F9] w-[375px] h-[52px]'> 
+            <div className='flex px-[15px] md:px-[20px] rounded-lg text-[18px] justify-center items-center bg-[#EFF1F9] h-[52px]'> 
               <IoMailOutline />
-              <input {...formik.getFieldProps('email')} className='w-[303px] ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Email Address' type='email'/>
+              <input {...formik.getFieldProps('email')} className='ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Email Address' type='email'/>
             </div>
 
-            <div className='flex rounded-lg my-[20px] text-[18px] justify-center items-center bg-[#EFF1F9] w-[375px] h-[52px]'> 
+            <div className='flex px-[15px] md:px-[20px] rounded-lg my-[20px] text-[18px] justify-center items-center bg-[#EFF1F9] h-[52px]'> 
               <CiLock />
-              <input {...formik.getFieldProps('password')} className='w-[303px] ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Password' type='password'/>
+              <input {...formik.getFieldProps('password')} className='ml-[10px] h-[36px] outline-none bg-[#EFF1F9]' placeholder='Password' type='password'/>
             </div>
          </div>
 
-         <NavLink to={'/forget-password'}><p className='text-primary flex justify-end items-center px-[40px]'>Recover Password</p></NavLink>
+         <NavLink to={'/forget-password'}><p className='text-primary flex justify-end items-center px-[10px]'>Recover Password</p></NavLink>
           <div className='flex justify-center items-center flex-col'>
               <p className='py-[15px]'>Already have an account ? <NavLink to={'/login'}><span className='text-primary'> Login </span></NavLink>  </p>
               <button type="submit" className='bg-primary px-[20px] py-2 mt-[20px] rounded-md text-white text-[18px]'>Register</button>
