a205aa2dae91144d89692dff16cc5eb894cfcd9d
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-26 14:02:31 +0530

    toast error corrected

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-// import toast, { Toaster } from "react-hot-toast";
 
 const Protected = ({ component: Component }) => {
   const navigate = useNavigate();
@@ -8,14 +7,12 @@ const Protected = ({ component: Component }) => {
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
-      // toast.error("Login to Proceed ...!");
       navigate("/login");
     }
   }, [navigate]);
 
   return (
     <>
-      {/* <Toaster position="top-center" reverseOrder={false} /> */}
       <div>{localStorage.getItem("token") ? <Component /> : null}</div>
     </>
   );
